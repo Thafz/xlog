180cd5c485463282ce5de8361f0644d71fcb8924
Thafheem <thafu.safarulla@gmail.com>
2024-11-26 03:35:58 +0400

    new

---
+++ assets/js/whatsapp.js
@@ -0,0 +1,14 @@
+function sendwhatsapp() {
+    let number = "+971 50 516 4511";
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    var url = "https://wa.me/" + number + "?text="
+    + "Name : " + name + "%0a"
+    + "Email : " + email + "%0a"
+    + "Message : " + message + "%0a%0a";
+
+    window.open(url, '_blank').focus();
+}
